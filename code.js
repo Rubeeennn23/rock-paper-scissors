ba468ace2a9d2e9a8ac5a87e90544ddf935bae62
Rubeeennn23 <ruben.vidal1@gmail.com>
2023-06-23 16:46:51 +0200

    function creation

--- code.js
+++ code.js
@@ -1,33 +1,63 @@
 // Rock paper scissors 
 // 1 - The computer selects one of the options randomly: Rock, Paper OR Scissors
-    const Choice = ["rock","paper","scissors"];
+    const choice = ["rock","paper","scissors"];
     let computerChoice = getComputerChoice();
     function getComputerChoice() {
-        return Choice[Math.floor(Math.random()*3)];
+        return choice[Math.floor(Math.random()*3)];
     }
 // 2 - The player selects one of the options: Rock, Paper OR Scissors - we should make it case-insensitive
-let playerChoice=prompt("Choose rock, paper or scissors:");
-function round (playerChoice,computerChoice) {
+let playerChoiceCase=prompt("Choose rock, paper or scissors:");
+let playerChoice = playerChoiceCase.toLowerCase();
+
+// 3 - Write a function that plays a single round of the game, the function should have 2 parameters: playerSelection and computerSelection
+let winSentence = `You win, ${playerChoice} wins ${computerChoice}`;
+let looseSentence = `You loose, ${computerChoice} wins ${playerChoice}`;
+let drawSentence = `Try again, both showed ${playerChoice}`;
+let roundSentence;
+let playerCount = 0;
+let computerCount = 0;
+let round = 0;
+let result = `Player ${playerCount} vs Computer ${computerCount}`;
+let resultSentence = roundSentence + " - " + result;
+
+function roundPlay (playerChoice,computerChoice) {
     if (playerChoice === "rock" & computerChoice === "scissors") {
-        return "You win, rock beats paper"
+        roundSentence = winSentence
+        playerCount =+ 1
     } else if (playerChoice === "rock" & computerChoice === "paper") {
-        return "You loose, paper beats rock"
+        roundSentence = looseSentence
+        computerCount =+ 1
     } else if (playerChoice === "rock" & computerChoice === "rock") {
-        return "Draw, both are rocks"
+       roundSentence = drawSentence
     } else if (playerChoice === "paper" & computerChoice === "rock") {
-        return "You win, paper beats rock"
+        roundSentence = winSentence
+        playerCount =+ 1
     } else if (playerChoice === "paper" & computerChoice === "scissors") {
-        return "You loose, scissors beats paper"
+        roundSentence = looseSentence
+        computerCount =+ 1
     } else if (playerChoice === "paper" & computerChoice === "paper") {
-        return "Draw, both are papers"
+        roundSentence = drawSentence
     } else if (playerChoice === "scissors" & computerChoice === "paper") {
-        return "You win, scissors beats paper"
+        roundSentence = winSentence
+        playerCount =+ 1
     } else if (playerChoice === "scissors" & computerChoice === "rock") {
-        return "You loose, rock beats scissors"
+        roundSentence = looseSentence
+        computerCount =+ 1
     } else {
-        return "Draw, both are papers"
-    } return result
+        roundSentence = drawSentence
+    } return roundSentence+" - " + result
 }
+
+ alert(roundPlay(playerChoice,computerChoice));
   
-// 3 - The algorithm compares both sellections and returns a result based on those conditions: rock wins scissors, scissors wins paper, paper wins rock. If both selections are the same, the result will be draw
-// 
+// 4 - Write a new function called game(). Use the previous function inside of this one to play a 5 round game that keeps socre an reports a winner and looser at the end
+// We don't know how to create loops right now, repeat the function 5 times. 
+// We can display the result using console log of each round and the winner in the end
+// Use prompt() to get the input from the user
+// Change the previous function if needed, specually the return value 
+// If needed, create more helper functions that could be usefull
+
+//function game() {
+//    round(playerChoice,computerChoice)
+
+//}
